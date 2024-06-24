66c92cbfa659205ed613b10445304f479dce215c
hamza <hamzashwliap@gmail.com>
2024-06-24 16:43:41 +0300

    right down the logic  and initialize the variables

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+// create variables to store the wind and loses and draws//
+//create a variable to make and store the computes choice//
+//take the user answer and store it in a new variable //
+//compare the user answer to that of the computer and determine how won  //
+//repeat five times and and determine the winner //
