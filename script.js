31d0b8a55e753830d6f5678109238ddb911e27e3
hamza <hamzashwliap@gmail.com>
2024-06-25 14:58:33 +0300

    its connected to the last commit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 console.log("hello world");
-// create variables to store the wind and loses and draws//
+// create variables to store the wins and loses and draws//
+let computerWins =0
+let userWins =0
+let draws =0
 //create a variable to make and store the computes choice//
 //take the user answer and store it in a new variable //
+let userAnswer =prompt("rock , paper , scissors")
 //compare the user answer to that of the computer and determine how won  //
 //repeat five times and and determine the winner //
