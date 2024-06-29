803c4b1a25eb699a7bdabcba218f0d0a8a84c058
hamza <hamzashwliap@gmail.com>
2024-06-29 11:38:49 +0300

    create the function that takes the user answer and and finish the code

--- script.js
+++ script.js
@@ -3,14 +3,31 @@ console.log("hello world");
 let computerWins =0
 let userWins =0
 let draws =0
+let userAnswer =""
 //create a variable to make and store the computes choice//
-
-//compare the user answer to that of the computer and determine how won  //
-function compare (userAnswer ,comAnswer){
+let b = ["rock", "paper", "scissors"]
+comAnswer= ""
+function computerAnswer(){
+     comAnswer = b[Math.floor(Math.random()* b.length )]
+}
 //take the user answer and store it in a new variable //
-    let userAnswer =prompt("rock , paper , scissors")
+function humanAnswer(){
+userAnswer =prompt("rock , paper , scissors")
+console.log(userAnswer)
+
+if (userAnswer == 'rock' || 'paper'||'scissors'){
+    return (userAnswer)
+} else{
+    alert('answer not valid')
+    humanAnswer()
+}
+}
+//compare the user answer to that of the computer and determine who won  //
+function compare (){
+    computerAnswer()
+    humanAnswer()
     if (userAnswer == "rock" && comAnswer == "rock"){
-        draw +=1
+        draws +=1
     } else if (userAnswer == "rock" && comAnswer == "paper"){
         computerWins +=1
     }else if (userAnswer == "rock" && comAnswer == "scissors"){
@@ -18,7 +35,7 @@ function compare (userAnswer ,comAnswer){
     }else if (userAnswer == "paper" && comAnswer == "rock"){
         userWins +1
     }else if (userAnswer == "paper" && comAnswer == "paper"){
-        draw +=1
+        draws +=1
     }else if (userAnswer == "paper" && comAnswer == "scissors"){
         computerWins += 1
     }else if (userAnswer == "scissors" && comAnswer == "paper"){
@@ -28,21 +45,22 @@ function compare (userAnswer ,comAnswer){
     }else if (userAnswer == "scissors" && comAnswer == "scissors"){
         draws+=1
     }
-    alert("wins ="+userWins.toString()+" loses ="+computerWins.toString+" draws ="+draws.toString)
+    alert("wins ="+userWins.toString()+" loses ="+computerWins.toString()+" draws ="+draws.toString())
 }
 //a function that resets the game//
 function reset(userWins ,computerWins ,draws){
     userWins =0
     computerWins =0
-    draws =0 
+    draws =0
+    comAnswer="" 
 }
 //repeat five times and and determine the winner //
-function theGame (userAnswer ,comAnswer){
-    compare(userAnswer,comAnswer)
-    compare(userAnswer,comAnswer)
-    compare(userAnswer,comAnswer)
-    compare(userAnswer,comAnswer)
-    compare(userAnswer,comAnswer)
+function theGame (userAnswer ,){
+    compare()
+    compare()
+    compare()
+    compare()
+    compare()
     if (userWins > computerWins){
         alert('you won')
     }else if (computerWins > userWins){
@@ -50,4 +68,6 @@ function theGame (userAnswer ,comAnswer){
     }else {
         alert("it is a draw ")
     }
+    reset(userWins ,computerWins ,draws)
 }
+theGame()
